a31a514c6dcbb9ac58ce1a59dd674bd91a095fd9
Shagansran1 <Shagansran782@gmail.com>
2025-09-02 10:52:03 -0500

    completed tutorial 2

--- S2.js
+++ S2.js
@@ -14,9 +14,48 @@ const pi = 3.14;
 
 //let result = 5 * (10-3)**2;
 
-let likes = 10;
-//like = likes + 1;
-likes++
+//let likes = 10;
 
-console.log(likes);
-//hahaha
+// Short hand notations:
+
+//like = likes + 1;
+//likes++
+//likes += 10;
+//likes -= 2;
+//likes *= 2;
+//likes /= 2;
+
+//onsole.log(likes);
+
+
+// NaN - not a number
+
+//console.log(5 / 'helo');
+
+//let result = 'the blog has' + ' ' + likes + ' ' +'likes';
+//console.log(result);
+
+// Template strings
+
+const title = 'Best reads of 2020';
+const author = 'shagan';
+const liked = '30';
+
+//concatenation way
+
+//let result = 'The blog called ' + title + ' by ' + author + ' has ' + liked + ' likes ';
+//console.log(result)
+
+//template string way/template literal
+
+let result = `The blog called ${title}  by ${author} has ${liked} likes`;
+console.log(result);
+
+// creating html templates
+let html = ` 
+<h2> ${title}</h2>
+<p> By ${author}</p>
+<span>This blog has ${liked} likes </span>
+`;
+
+console.log(html);
---
+++ S3.js
@@ -0,0 +1,69 @@
+/*let nijas = ['shaun', 'ryu', 'chun-li'];
+
+
+nijas[1] = 'ken';
+;
+
+let ages = [20,25,30,35];
+console.log(ages[2]);
+
+console.log(nijas.length);
+// how to join
+//let result = nijas.join(',');
+
+//let result = nijas.indexOf('chun-li');
+//let result = nijas.concat(['ken', 'crystal']);
+//let result = nijas.push('ken'); return the lenght of new array
+//arrays alter the original value
+result = nijas.pop();
+
+console.log(result);*/
+
+/*let age = null; 
+
+console.log(age, age + 3, `the age is ${age}`);
+
+
+//Boolean
+
+//console.log(true, false);
+
+//methods can return booleans
+let email = 'shaganprdhaan.com';
+let names = ['mario', 'luigi', 'toad'];
+let result = email.includes('@');
+let result2 = names.includes('toad');
+
+
+//console.log(result);
+//console.log(result2);
+//comparison operators
+let age = 25;
+
+console.log( age == 25);
+console.log(age != 30); //!= not equal
+console.log( age > 20);
+console.log (age < 20);
+console.log( age <= 25); //<= , >=
+
+let names = 'shaun';
+
+console.log(names == 'shaun');/
+
+// strict comparison
+let age = 25;
+console.log(age === 25);
+console.log(age === '25'); //number is not equal to string
+
+
+let score = '100';
+
+score = Number(score);
+console.log(score + 1);
+console.log(typeof score);*/
+
+//let result = Boolean('');
+
+//console.log(result, typeof result);
+
+
